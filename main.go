b28a8bac25c26b49b2505026659adad32e754494
iselegant <masar@synspective.com>
2023-09-08 11:40:12 +0900

    Update: v1.0.2

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-const version = "1.0.1" // This value will be rewritten after gobump execute
+const version = "1.0.2" // This value will be rewritten after gobump execute
 
 func main() {
 	fmt.Println("hello world")
